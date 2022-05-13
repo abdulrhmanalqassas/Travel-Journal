ca4e31120eaec6eb42c5bafb3c26a7ff1349f084
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2022-05-13 16:03:40 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Nav from "./componant/Nav";
+import "./index.css";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Nav/>);
+
+// ReactDOM.render(document.getElementById("root"),<Card/>)
